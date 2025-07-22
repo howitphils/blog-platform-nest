c21f9fac5e2842cde5fc08fe23a6b4c636ecb8b8
howitphils <uladzislauhr@gmail.com>
2025-07-22 18:48:47 +0300

    added options to enableCors method

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,18 @@ import { appConfig } from './app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'domain-name',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(appConfig.PORT);
