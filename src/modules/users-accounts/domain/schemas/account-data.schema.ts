c66e60395041816947f39a45c4f15fdb57448e2c
howitphils <uladzislauhr@gmail.com>
2025-07-29 19:32:06 +0300

    added email-confirmation + account-data schemas to user entity

---
+++ src/modules/users-accounts/domain/schemas/account-data.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ _id: false })
+export class AccountData {
+  @Prop({
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 1,
+    maxlength: 20,
+  })
+  login: string;
+
+  @Prop({ type: String, required: true })
+  passwordHash: string;
+
+  @Prop({ type: String, required: true, minlength: 6, maxlength: 100 })
+  email: string;
+
+  // Дата для софт удаления
+  @Prop({ type: Date, nullable: true, default: null })
+  deletedAt: Date | null;
+
+  /**
+   * Creation timestamp
+   * Explicitly defined despite timestamps: true
+   * properties without @Prop for typescript so that they are in the class instance (or in instance methods)
+   * @type {Date}
+   */
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const UserAccountDataSchema = SchemaFactory.createForClass(AccountData);
--- src/modules/users-accounts/domain/user.entity.ts
+++ src/modules/users-accounts/domain/user.entity.ts
@@ -1,45 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Model } from 'mongoose';
 import { CreateUserDomainDto } from './dto/create-user.domain.dto';
+import {
+  AccountData,
+  UserAccountDataSchema,
+} from './schemas/account-data.schema';
+import {
+  EmailConfirmation,
+  EmailConfirmationSchema,
+} from './schemas/email-confirmation.schema';
 
 @Schema({ timestamps: true, collection: 'users' })
 export class User {
-  @Prop({
-    type: String,
-    unique: true,
-    required: true,
-    minlength: 1,
-    maxlength: 20,
-  })
-  login: string;
+  @Prop({ type: UserAccountDataSchema })
+  accountData: AccountData;
 
-  @Prop({ type: String, required: true })
-  passwordHash: string;
-
-  @Prop({ type: String, required: true, minlength: 6, maxlength: 100 })
-  email: string;
-
-  @Prop({ type: Boolean, required: true, default: false })
-  isEmailConfirmed: boolean;
-
-  // Дата для софт удаления
-  @Prop({ type: Date, nullable: true, default: null })
-  deletedAt: Date | null;
-
-  /**
-   * Creation timestamp
-   * Explicitly defined despite timestamps: true
-   * properties without @Prop for typescript so that they are in the class instance (or in instance methods)
-   * @type {Date}
-   */
-  createdAt: Date;
-  updatedAt: Date;
+  @Prop({ type: EmailConfirmationSchema })
+  emailConfirmation: EmailConfirmation;
 
   static createUser(dto: CreateUserDomainDto): UserDbDocument {
     const user = new this();
-    user.email = dto.email;
-    user.passwordHash = dto.passwordHash;
-    user.login = dto.login;
+    user.accountData.email = dto.email;
+    user.accountData.passwordHash = dto.passwordHash;
+    user.accountData.login = dto.login;
 
     return user as UserDbDocument;
   }
@@ -51,10 +34,10 @@ export class User {
    * DDD continue: инкапсуляция (вызываем методы, которые меняют состояние\св-ва) объектов согласно правилам этого объекта
    */
   makeDeleted() {
-    if (this.deletedAt !== null) {
+    if (this.accountData.deletedAt !== null) {
       throw new Error('Entity already deleted');
     }
-    this.deletedAt = new Date();
+    this.accountData.deletedAt = new Date();
   }
 }
 
