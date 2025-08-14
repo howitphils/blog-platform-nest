2a76a789f9df3eca1da7bf84ecd469fe53d01f9e
howitphils <uladzislauhr@gmail.com>
2025-08-14 16:02:25 +0300

    added trust proxy true

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { appSetup } from './setup/app.setup';
 import { CoreConfig } from './core/core.config';
 import { initAppModule } from './init-app-module';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const dynamicAppModule = await initAppModule();
 
-  const app = await NestFactory.create(dynamicAppModule);
+  const adapter = new ExpressAdapter();
+  adapter.set('trust proxy', true);
+
+  const app = await NestFactory.create(dynamicAppModule, adapter);
 
   const coreConfig = app.get<CoreConfig>(CoreConfig);
+
   appSetup(app);
 
   await app.listen(coreConfig.port);
