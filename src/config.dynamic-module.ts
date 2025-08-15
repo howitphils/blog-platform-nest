ad0bedfb3b734d36f906170a0e3326e0894d5092
howitphils <uladzislauhr@gmail.com>
2025-08-15 08:53:35 +0300

    added env folder and changed pathing to files

--- src/config.dynamic-module.ts
+++ src/config.dynamic-module.ts
@@ -1,11 +1,12 @@
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
     process.env.ENV_FILE_PATH?.trim() || '',
-    `.env.${process.env.NODE_ENV}.local`,
-    `.env.${process.env.NODE_ENV}`,
-    '.env.production',
+    join(__dirname, 'env', `.env.${process.env.NODE_ENV}.local`),
+    join(__dirname, 'env', `.env.${process.env.NODE_ENV}`),
+    join(__dirname, 'env', '.env.production'),
   ],
   isGlobal: true,
 });
